2d1ed2588661839c238debee9f8ad986251e8d03
arvindpai <arvindmpai@gmail.com>
2019-11-21 11:29:13 -0800

    Added hana ha svpc with network module

---
+++ modules/sap_hana_ha_svpc_with_network/firewall.tf
@@ -0,0 +1,73 @@
+/**
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+# Creation of different firewall rules
+
+resource "google_compute_firewall" "ssh" {
+  name    = "${var.network}-firewall-ssh"
+  project = "${var.host_project_id}"
+  network = "${google_compute_network.gcp_network.name}"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  target_tags   = ["${var.network}-firewall-ssh"]
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "http" {
+  name    = "${var.network}-firewall-http"
+  project = "${var.host_project_id}"
+  network = "${google_compute_network.gcp_network.name}"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  target_tags   = ["${var.network}-firewall-http"]
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "https" {
+  name    = "${var.network}-firewall-https"
+  project = "${var.host_project_id}"
+  network = "${google_compute_network.gcp_network.name}"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  target_tags   = ["${var.network}-firewall-https"]
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "icmp" {
+  project = "${var.host_project_id}"
+  name    = "${var.network}-firewall-icmp"
+  network = "${google_compute_network.gcp_network.name}"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  target_tags   = ["${var.network}-firewall-icmp"]
+  source_ranges = ["0.0.0.0/0"]
+}
